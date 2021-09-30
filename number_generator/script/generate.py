b31dd9c1c7ef4b3aeca41e03ab16da82d21eb82a
Wieland, Trevor | Trevor | RMI <trevor_wieland@mac.com>
2021-09-30 17:40:22 +0900

    Finished initial Image Processing
    Added support for generating an image sequence from MNIST digits

--- number_generator/script/generate.py
+++ number_generator/script/generate.py
@@ -1,5 +1,6 @@
 from typing import Iterable, Tuple, Optional
 from .dataload import MNIST_Loader
+from .imageprocessor import invert_image, space_images, pad_image_bounds
 import numpy as np
 
 def generate_numbers_sequence(
@@ -36,7 +37,17 @@ def generate_numbers_sequence(
     dimension to the width.
     """
 
-    return np.ones((1,1))
+    mnist = MNIST_Loader(random_seed=random_seed)
+
+    mnist.load_MNIST()
+
+    images = [mnist.fetch_digit(d) for d in digits]
+
+    image = space_images(images, spacing_range, random_seed=random_seed)
+    image = pad_image_bounds(image, image_width, random_seed=random_seed)
+    image = invert_image(image)
+
+    return image
 
 def generate_phone_numbers(
     spacing_range: Tuple[int, int],
---
+++ number_generator/script/imageprocessor.py
@@ -0,0 +1,123 @@
+from typing import List, Tuple, Optional
+import numpy as np
+
+def invert_image(image: np.ndarray) -> np.ndarray:
+    """
+    Inverts the color of an image for readability
+
+    Parameters
+    ----------
+    image:
+        A single image in numpy matrix format. Each cell should be a value 0-255.
+
+    Returns
+    -------
+    image:
+        The same image as given, but with each pixel invert in color
+    """
+
+    return 255 - image
+
+
+def trim_image(image: np.ndarray) -> np.ndarray:
+    """
+    Trims an image in numpy format. Removes all horizontal empty (0) space.
+
+    Parameters
+    ----------
+    image:
+        A single image in numpy matrix format. Each cell should be a value 0-255.
+
+    Returns
+    -------
+    image:
+        The same image as given, but trimmed so that all horizontal empty space
+        is gone.
+    """
+
+    maxs = np.max(image, axis=0)
+    nonzero = np.nonzero(maxs)[0]
+
+    new_image = image[:, nonzero[0]:nonzero[-1]+1]
+
+    return new_image
+
+
+def space_images(
+    images: List[np.ndarray],
+    spacing_range: Tuple[int, int],
+    random_seed: Optional[int] = None
+) -> np.ndarray:
+    """
+    Takes a lsit of numpy image matrices and spaces them in a single image,
+    with random buffers between each in the given `spacing_range`.
+
+    Parameters
+    ----------
+    images:
+        A list of images, in numpy matrix format.
+    spacing_range:
+        A tuple of integers (min, max) to uniform draw spacings from.
+    random_seed:
+        An optional int to feed to the random number generator for consistency:
+        Default is None.
+
+    Returns
+    -------
+    image, a combined numpy ndarray that has all digits in it, with uniform
+    random spacing between each digit.
+    """
+
+    rng = np.random.default_rng(random_seed)
+
+    images = [trim_image(img) for img in images]
+
+    grand_image_list = [images[0]]
+
+    for i in range(1, len(images)):
+        random_space = rng.integers(low=spacing_range[0], high=spacing_range[1]+1, size=1)[0]
+
+        grand_image_list.append(np.zeros((28, random_space)))
+        grand_image_list.append(images[i])
+
+    image = np.concatenate(grand_image_list, axis=1)
+
+    return image
+
+def pad_image_bounds(
+    image: np.ndarray,
+    image_width: int,
+    random_seed: Optional[int] = None
+) -> np.ndarray:
+    """
+    Pads the left and right of the image with extra empty space, to reach the
+    target `image_width`.
+
+    Parameters
+    ----------
+    image:
+        An image in numpy matrix format
+    image_width:
+        An integer number of pizels the image should be in total
+    random_seed:
+        An optional int to feed to the random number generator for consistency:
+        Default is None.
+
+    Returns
+    -------
+    image, a numpy matrix image with the desired target width.
+
+    """
+
+    rng = np.random.default_rng(random_seed)
+
+    current_width = image.shape[1]
+    remaining_width = image_width - current_width
+
+    left_width = rng.integers(low=0, high=remaining_width, size=1)[0]
+    right_width = remaining_width - left_width
+
+    images = [np.zeros((28, left_width)), image, np.zeros((28, right_width))]
+    image = np.concatenate(images, axis=1)
+
+    return image
